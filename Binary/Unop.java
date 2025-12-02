98e29724a501eee6bf569c169cb471298cf72b61
Francine Lohose <flohose503@wcupa.edu>
2025-12-02 11:24:46 -0800

    Delete Binary directory

--- Binary/Unop.java
+++
@@ -1,7 +0,0 @@
-/**
- * Code Template Author: David G. Cooper
- * Purpose: A base class for unary operations
- */
-public abstract class Unop extends Op {
-     public abstract double eval(double[] values);
-}
