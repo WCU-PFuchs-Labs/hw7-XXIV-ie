4bf2eb9c8724507751275e44a0f4183881b4bce8
Francine Lohose <flohose503@wcupa.edu>
2025-12-02 11:15:55 -0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,10 +1,5 @@
-package Binary;
-
 import java.util.Random;
-/**
- * Code Template Author: David G. Cooper
- * Purpose: A Binary Tree class for Arithmetic evaluation
- */
+
 public class Node {
     private Node left;
     private Node right;
@@ -15,17 +10,19 @@ public class Node {
         this.operation = operation;
         this.depth = 0;
     }
+
     public Node(Op operation) {
         this.operation = operation;
         this.depth = 0;
     }
-    public Node (Binop operation, Node left, Node right){
+
+    public Node(Binop operation, Node left, Node right) {
         this.operation = operation;
         this.depth = 0;
         attachLeft(left);
         attachRight(right);
     }
-    // Helper methods to attach children and set their depth correctly
+
     private void attachLeft(Node child) {
         this.left = child;
         if (child != null) {
@@ -39,15 +36,16 @@ public class Node {
             child.setDepth(this.depth + 1);
         }
     }
-    public int getDepth(){
+
+    public int getDepth() {
         return this.depth;
     }
+
     public void setDepth(int depth) {
         this.depth = depth;
     }
 
     public void addRandomKids(NodeFactory nf, int maxDepth, Random rand) {
-
         if (!(operation instanceof Binop)) {
             return;
         }
@@ -57,8 +55,9 @@ public class Node {
             attachRight(nf.getTerminal(rand));
             return;
         }
+
         int choiceRange = nf.getNumOps() + nf.getNumIndepVars();
-        //left Child
+
         int pickLeft = rand.nextInt(choiceRange + 1);
         if (pickLeft < nf.getNumOps()) {
             Node L = nf.getOperator(rand);
@@ -67,7 +66,7 @@ public class Node {
         } else {
             attachLeft(nf.getTerminal(rand));
         }
-        //right child
+
         int pickRight = rand.nextInt(choiceRange + 1);
         if (pickRight < nf.getNumOps()) {
             Node R = nf.getOperator(rand);
@@ -81,49 +80,7 @@ public class Node {
     public Object clone() {
         Object o = null;
         try {
-            o =  super.clone();
+            o = super.clone();
         }
         catch(CloneNotSupportedException e) {
-            System.out.println("Op can't clone.");
-        }
-        Node b = (Node) o;
-        if(left != null) {
-            b.left = (Node) left.clone();
-        }
-        if(right != null) {
-            b.right = (Node) right.clone();
-        }
-        if(operation != null) {
-            b.operation = (Op) operation.clone();
-        }
-        return b;
-    }
-
-    public double eval(double[] values) {
-        if (operation instanceof Unop) {
-            return ((Unop) operation).eval(values);
-        } else if (operation instanceof Binop) {
-            double leftVal = left.eval(values);
-            double rightVal = right.eval(values);
-
-            return ((Binop) operation).eval(leftVal, rightVal);
-        } else {
-            System.err.println("Error operation is not a Unop or a Binop!");
-            return 0.0;
-        }
-    }
-
-    //Add a toString
-    public String toString() {
-        if (operation instanceof Unop) {
-            return  operation.toString() ;
-        }
-        if (operation instanceof Binop) {
-            return "("+ left.toString() + " " + operation.toString() + " " + right.toString()+ ")";
-        } else {
-            return "";
-        }
-    }
-}
-
-
+            System.out.println("Op can'
