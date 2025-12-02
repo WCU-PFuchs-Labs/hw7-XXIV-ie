3c2fd9d64baa311ed294d94a5b42b267b68272b4
Francine Lohose <flohose503@wcupa.edu>
2025-12-02 11:23:40 -0800

    Delete Binary/Divide.java

--- Binary/Divide.java
+++
@@ -1,9 +0,0 @@
-public class Divide extends Binop {
-    public double eval(double left, double right) {
-       double quotient = left / right;
-       return (double) Math.round(quotient*100)/100;
-    }
-    public String toString() {
-        return "/";
-    }
-}
