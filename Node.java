ca3614988457d99466d3da7b0ae228954fe23fa2
Francine Lohose <flohose503@wcupa.edu>
2025-12-02 15:34:41 -0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -29,10 +29,8 @@ public class Node {
 
     public double eval(double [] values) {
         if (op instanceof Unop) {
-            // Terminal node - delegate to Unop
             return ((Unop) op).eval(values);
         } else {
-            // Binary operation node
             double leftVal = left.eval(values);
             double rightVal = right.eval(values);
             return ((Binop) op).eval(leftVal, rightVal);
@@ -52,12 +50,10 @@ public class Node {
     }
 
     public void addRandomKids (NodeFactory factory, int maxDepth, Random rand) {
-        // If this is a terminal node(Unop), do nothing
         if (op instanceof Unop) {
             return;
         }
 
-        // If we've reached max depth, add terminals for both children
         if (this.depth == maxDepth) {
             this.left = factory.getTerminal(rand);
             this.left.setDepth(this.depth + 1);
@@ -67,32 +63,25 @@ public class Node {
             return;
         }
 
-        // Otherwise, continue growing the tree
         int numOps = factory.getNumOps();
         int numIndepVars = factory.getNumIndepVars();
 
-        //add left child
         int leftChoice = rand.nextInt(numOps + numIndepVars);
         if (leftChoice < numOps) {
-            // add operator as left child
             this.left = factory.getOperator(rand);
             this.left.setDepth(this.depth + 1);
             this.left.addRandomKids(factory, maxDepth, rand);
         } else {
-            // Add terminal as left child
             this.left = factory.getTerminal(rand);
             this.left.setDepth(this.depth + 1);
         }
 
-        // add right child
         int rightChoice = rand.nextInt(numOps + numIndepVars);
         if (rightChoice < numOps) {
-            // add operator as right child
             this.right = factory.getOperator(rand);
             this.right.setDepth(this.depth + 1);
             this.right.addRandomKids(factory, maxDepth, rand);
         } else {
-            // add terminal as right child
             this.right = factory.getTerminal(rand);
             this.right.setDepth(this.depth + 1);
         }
@@ -128,20 +117,16 @@ public class Node {
 
     public String toString() {
         if (op instanceof Unop) {
-            // terminal node
             return op.toString();
         } else {
-            // Binary operation nodee
             return "(" + left.toString() + op.toString() + right.toString() + ")";
         }
     }
 
-    // setter for depth
     public void setDepth(int depth) {
         this.depth = depth;
     }
 
-    // Getter for depth
     public int getDepth() {
         return this.depth;
     }
