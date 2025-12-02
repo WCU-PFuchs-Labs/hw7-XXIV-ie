749befb211451ebaf24692826818e1fd10e68099
Francine Lohose <flohose503@wcupa.edu>
2025-12-02 11:29:31 -0800

    Update Op.java

--- Op.java
+++ Op.java
@@ -4,14 +4,11 @@
  */
 public abstract class Op implements Cloneable {
     public Object clone() {
-        Object o = null;
         try {
-            o =  super.clone();
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
         }
-        catch(CloneNotSupportedException e) {
-            System.out.println("Op can't clone.");
-        }
-        return o;
     }
 }
 
